87ed0a75ebb799e67f3c006cffea84e63692a588
AlejoRetola <alejoretola2001@gmail.com>
2025-05-18 15:56:58 -0300

    Saque el server

--- alquilerexpress/Server/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const { mostrar_todos_inmuebles } = require('../controllers/inmueble_controller')
-
-const app = require('express').Router();
-
-app.get('/', mostrar_todos_inmuebles);
-
-module.exports = app;
